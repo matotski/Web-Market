79f844a9b6870ace0b01624ce77ba11a9e5ef6d5
matotski <matotski@mail.ru>
2024-12-19 00:50:25 +0300

    Add reset password functional for login

--- src/config/urls.py
+++ src/config/urls.py
@@ -19,13 +19,19 @@ from django.urls import path, include
 from products.views import main
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
     path('products/', include('products.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
